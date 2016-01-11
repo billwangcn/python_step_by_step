c0792e6d4d6a8bbb7f48240ff27f10bdfe528666
bill <wangbinpq@163.com>
2016-01-11 15:41:19 +0800

    修改：     README.md 	修改：     first.py 	修改：     main.py

--- first.py
+++ first.py
@@ -1,16 +1,19 @@
-:#-*- coding:utf-8 -*-
+#-*- coding:utf-8 -*-
 import os,sys
-import uuit
+import uuid
 
 def getSysInfo():
+    print(os.getcwd())
     return 'null'
 
 def getSum(i):
     s=0
+    for a in range(1,i+1):
+        s=s+a
     return s
 
 def getUUID():
-    return uuid.UUID4()
+    return uuid.UUID()
 
 
 
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 #-*- coding:utf-8 -*-
 import os,sys
+import first
+
 def getSysInfo():
     return 'null'
 
+def loadfile(path):
+    file = open(path)
+    return file
+
 if __name__=='__main__':
-	getSysInfo()
+    #getSysInfo()
+    print(first.getUUID())
 
 
 
